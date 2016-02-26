e53bb21afb792746fb894645ab0dd9aae3f010b8
llafuente <llafuente@noboxout.com>
2016-02-26 12:45:09 +0100

    update node 4.3.1

--- node.sh
+++ node.sh
@@ -5,18 +5,13 @@ cd /tmp/node/
 
 # legacy node
 # wget https://nodejs.org/dist/v0.10.40/node-v0.10.40-linux-x64.tar.gz
-wget https://nodejs.org/dist/v4.2.6/node-v4.2.6-linux-x64.tar.gz
+wget https://nodejs.org/dist/v4.3.1/node-v4.3.1-linux-x64.tar.gz
 tar xsfv node-*-linux-*.tar.gz
 cd node-*-linux-*
 
-# io.js
-#wget https://iojs.org/dist/v3.3.0/iojs-v3.3.0-linux-x64.tar.xz
-#tar xsfv iojs-*-linux-*.tar.xz
-#cd iojs-*-linux-*
-
-rm -f ChangeLog
+rm -f CHANGELOG.md
 rm -f LICENSE
-rm -f README.mkdir
+rm -f README.md
 sudo cp -rf * /usr/local/
 
 sudo /usr/local/bin/npm install -g yo grunt-cli browserify uglify-js bower

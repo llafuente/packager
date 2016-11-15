d1584e910bb0cd2d5f429f0225c7c05ab566ff63
llafuente <llafuente@noboxout.com>
2016-11-15 23:08:44 +0100

    monit fixes

--- packages/monit.sh
+++ packages/monit.sh
@@ -12,4 +12,22 @@ sudo cp -rf "${INSTALLER_PATH}/monit/nginx" /etc/monit.d/nginx
 sudo cp -rf "${INSTALLER_PATH}/monit/php-fpm" /etc/monit.d/php-fpm
 
 
+cat <<DELIM | sudo tee /etc/yum.repos.d/MariaDB.repo
+set daemon  60
+set logfile syslog
+
+set logfile /var/log/monit.log
+
+set httpd port 2812 and
+    use address 0.0.0.0    # listen any IP
+    allow 0.0.0.0/0.0.0.0  # allow localhost to connect to the server and
+    allow admin:monit      # require user 'admin' with password 'monit'
+
+include /etc/monit.d/*
+
+DELIM
+
+sudo systemctl enable monit
+sudo systemctl start monit
+
 echo "OK"

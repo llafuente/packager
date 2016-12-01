5b7ceda2c91de2458b0e6be799a09a18d5485519
llafuente <llafuente@noboxout.com>
2016-12-01 16:54:56 +0100

    static website minify html

--- aws/sync-static-website.sh
+++ aws/sync-static-website.sh
@@ -17,7 +17,6 @@ case $i in
 esac
 done
 
-
 if [ -z ${SOURCE} ]; then
   echo "--source is required"
   echo "KO"
@@ -30,6 +29,16 @@ if [ -z ${DOMAIN} ]; then
   exit 1
 fi
 
+#install minifier if needed!
+if [ -z `which html-minifier` ]; then
+  npm install -g html-minifier
+  if [ -z `which html-minifier` ]; then
+    echo "html-minifier cannot be installed"
+    exit 1
+  fi
+fi
+
+
 #public/index.html: HTML document, UTF-8 Unicode text
 #public/index.html: gzip compressed data, was "index.html", \
 #from Unix, last modified: Tue Nov 22 16:43:35 2016, max compression
@@ -39,7 +48,11 @@ IS_COMPRESSED=$(file "${SOURCE}/index.html" | grep 'gzip')
 if [ -z ${IS_COMPRESSED} ]; then
   echo "compressing source path"
 
+  # minify
+  find ${SOURCE} -name '*.html' | xargs -I '{}' sh -c "html-minifier --collapse-whitespace --remove-tag-whitespace '{}' | sponge '{}'"
+  # gzip
   find ${SOURCE} | xargs gzip -9
+  # rename .*.gz .*
   find ${SOURCE} -type f -name '*.gz' | \
     while read f; do mv "$f" "${f%.gz}"; done
 fi

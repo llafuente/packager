1e770c7d408aaa91cb2cb0a3b7e438c843d75adf
llafuente <llafuente@noboxout.com>
2017-08-02 13:23:32 +0200

    another fix for nginx

--- packages/disable-selinux.sh
+++ packages/disable-selinux.sh
@@ -23,4 +23,8 @@ fi
 # this fix nginx - php-fpm
 sudo setenforce 0
 
+# this fix another nginx error with reverse-proxy
+# 2017/08/02 11:19:11 [crit] 25186#0: *26 connect() to [::1]:8080 failed (13: Permission denied) while connecting to upstream, client: ***, server: , request: "GET / HTTP/1.1", upstream: "http://[::1]:8080/", host: "***"
+sudo setsebool -P httpd_can_network_connect 1
+
 echo "OK"

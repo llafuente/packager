4044e068a5d8dd969a8d7bf49b1789536c661235
llafuente <llafuente@noboxout.com>
2017-10-17 15:31:40 +0200

    Update disable-selinux.sh

--- packages/disable-selinux.sh
+++ packages/disable-selinux.sh
@@ -27,4 +27,8 @@ sudo setenforce 0
 # 2017/08/02 11:19:11 [crit] 25186#0: *26 connect() to [::1]:8080 failed (13: Permission denied) while connecting to upstream, client: ***, server: , request: "GET / HTTP/1.1", upstream: "http://[::1]:8080/", host: "***"
 sudo setsebool -P httpd_can_network_connect 1
 
+# under normal circunstances to really stop the selinux need a reboot
+# this get info about the running selinux config, should be: SELinux status:                 disabled
+/usr/sbin/sestatus
+
 echo "OK"
